67ae5ca79bacfb959898ca91d1d2777090f4319f
EmirBozan <99737163+EmirBozan@users.noreply.github.com>
2023-06-14 20:00:12 +0300

    Add files via upload

---
+++ project21.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class project21 {
+    public static void main(String[] args) {
+        int deger, us, sonuc=1, taban;
+        Scanner sayi = new Scanner(System.in);
+        System.out.print("Sonucunu istediğiniz üslü sayının tabanını yazınız : ");
+        taban= sayi.nextInt();
+        System.out.print("Sonucunu istediğiniz üslü sayının üssünü yazınız : ");
+        us= sayi.nextInt();
+        for (int i=1; i<=us; i++){
+           sonuc=sonuc*taban ;
+        }
+        System.out.println(us1);
+
+
+
+
+
+    }
+}
